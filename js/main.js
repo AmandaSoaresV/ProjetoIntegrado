e657dd484df26b119dcd8e3c753d09098507fa0b
amandasoaresv <amandasoaresvieira5@gmail.com>
2026-03-29 15:02:49 -0300

    refactor: organiza arquivos JS em módulos

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { clickMenu } from "./modules/menu.js";
+import { initAcordion } from "./modules/accordion.js";
+
+initAcordion();
+clickMenu();
+
+window.clickMenu = clickMenu;
---
+++ js/modules/menu.js
@@ -0,0 +1,4 @@
+export function clickMenu() {
+  const menu = document.querySelector(".header-nav");
+  menu.classList.toggle("ativo");
+}
