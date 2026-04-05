4bb88857a64c288700547bd655e88f01331ef208
amandasoaresv <amandasoaresvieira5@gmail.com>
2026-04-05 15:17:39 -0300

    feat: adiciona alerta de lançamento do jogo com a biblioteca "SweetAlert2"

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { clickMenu } from "./modules/menu.js";
 import { initAcordion } from "./modules/accordion.js";
 import "./modules/darkmode.js";
+import "./modules/lancamento.js";
 
 initAcordion();
 
---
+++ js/modules/lancamento.js
@@ -0,0 +1,30 @@
+import Swal from "https://cdn.jsdelivr.net/npm/sweetalert2@11/+esm";
+
+// verifica se está na index.html
+if (window.location.pathname.includes("index.html") || window.location.pathname === "/") {
+
+  const anoLancamento = 2026;
+  const anoAtual = new Date().getFullYear();
+
+  if (anoAtual === anoLancamento) {
+    Swal.fire({
+      title: "O grande lançamento acabou de acontecer!",
+      text: "Seu jogo já está disponível!",
+      icon: "success",
+      confirmButtonText: "Jogar agora",
+    });
+  } else if (anoAtual < anoLancamento) {
+    Swal.fire({
+      title: "Em breve o jogo será lançado!",
+      text: `Faltam ${anoLancamento - anoAtual} ano(s)!`,
+      icon: "info",
+    });
+  } else {
+    Swal.fire({
+      title: "📀 Já foi lançado!",
+      text: "Confira o jogo agora!",
+      icon: "warning",
+    });
+  }
+
+}
