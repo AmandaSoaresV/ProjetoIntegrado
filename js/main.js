084c750c4b7e149ef637ba72fb86643b4d10bee8
amandasoaresv <amandasoaresvieira5@gmail.com>
2026-04-05 15:39:57 -0300

    feat: implementa alerta personalizado para o usuário ao enviar o formulário

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { clickMenu } from "./modules/menu.js";
 import { initAcordion } from "./modules/accordion.js";
 import "./modules/darkmode.js";
 import "./modules/lancamento.js";
+import "./modules/formulario.js";
 
 initAcordion();
 
---
+++ js/modules/formulario.js
@@ -0,0 +1,16 @@
+import Swal from "https://cdn.jsdelivr.net/npm/sweetalert2@11/+esm";
+const formulario = document.getElementById("formulario");
+
+if (formulario) {
+  formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const nome = document.getElementById("nome").value;
+     Swal.fire({
+      title: "Formulário Enviado!",
+      text: "Obrigada por entrar em contato, " + nome + "! Em breve responderemos sua mensagem.",
+      icon: "success",
+      confirmButtonText: "OK",
+    });
+    formulario.reset();
+  });
+}
